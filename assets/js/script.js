be7df771cbbc32df5eb741aa6245ef270a507c18
lucyinthesky2111 <lucy.kidner@outlook.com>
2025-04-09 21:12:23 +0100

    Add code to calculate two random numbers for the game question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,16 @@
-// Wait for the DOM to finish loading before starting the game
+/* Wait for the DOM to finish loading before starting the game. Need to do this, otherwise you could be trying to target 
+elements on the page that do not currently exist because they have not yet loaded. */
 // Get the button elements and add event listeners to them 
+
+/* If else statement returns first alert if the data type attribute is strictly equal to submit. If the data type attribute is 
+anything else e.g.  + - / * then the second alert is displayed. the template literal grabs the relevant name to be used e.g. 
+You clicked additon etc */
 
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button")
 
+    /* for (let button of buttons) is the same code as for (let i= 0; i < buttons.length; i++) but is a more efficient and
+     modern way of writing it */
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
@@ -17,9 +24,10 @@ document.addEventListener("DOMContentLoaded", function() {
         })
     }
 })
-
+  
 function runGame() {
-
+    let num1 = Math.floor(Math.random() *25 + 1)
+    let num2 = Math.floor(Math.random() *25 + 1)
 }
 
 function checkAnswer() {
